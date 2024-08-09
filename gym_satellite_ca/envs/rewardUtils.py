65add0426797a5aa0484054804e7f5420595f22d
Alex <alexandru.solomon9999@gmail.com>
2024-08-09 18:58:56 +0300

    Updated the reward system taking into account distance from earth's center and the keplerian elements of the orbits

--- gym_satellite_ca/envs/rewardUtils.py
+++ gym_satellite_ca/envs/rewardUtils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math
+
+from org.orekit.orbits import KeplerianOrbit
 
 
 class RewardUtils:
@@ -11,8 +14,46 @@ class RewardUtils:
     MAX_PUNISHMENT_OUT_BOUND = 10
     FUEL_USED_NORM_TERM = 10.0
 
+    # Orbital differences allowed between the initial orbit and final orbit
+    MAX_SMA_DIFF = 800.0  # meters
+    MAX_ECC_DIFF = 0.001
+    MAX_INC_DIFF = 2.0  # deg
+    MAX_PAR_DIFF = 5.0  # deg
+    MAX_RAN_DIFF = 5.0  # deg
+
     def __init__(self):
         pass
+
+    @staticmethod
+    def get_diff_between_kepl_elem(kepl_orbit_1: KeplerianOrbit, kepl_orbit_2: KeplerianOrbit):
+
+        # get the differences between keplerian elements
+        sma_diff = abs(kepl_orbit_1.getA() - kepl_orbit_2.getA())
+        ecc_diff = abs(kepl_orbit_1.getE() - kepl_orbit_2.getE())
+        inc_diff = abs(kepl_orbit_1.getI() - kepl_orbit_2.getI())
+        par_diff = abs(kepl_orbit_1.getPerigeeArgument() - kepl_orbit_2.getPerigeeArgument())
+        ran_diff = abs(kepl_orbit_1.getRightAscensionOfAscendingNode() - kepl_orbit_2.getRightAscensionOfAscendingNode())
+
+        return sma_diff, ecc_diff, inc_diff, par_diff, ran_diff
+
+    def compare_kepl_orbits(self, kepl_orbit_1: KeplerianOrbit, kepl_orbit_2: KeplerianOrbit):
+        sma_diff, ecc_diff, inc_diff, par_diff, ran_diff = self.get_diff_between_kepl_elem(
+            kepl_orbit_1, kepl_orbit_2
+        )
+        reward = 0
+
+        if sma_diff > self.MAX_SMA_DIFF:
+            reward -= 1.0
+        if ecc_diff > self.MAX_ECC_DIFF:
+            reward -= 1.0
+        if inc_diff > self.MAX_INC_DIFF:
+            reward -= 1.0
+        if par_diff > self.MAX_PAR_DIFF:
+            reward -= 1.0
+        if ran_diff > self.MAX_RAN_DIFF:
+            reward -= 1.0
+
+        return reward
 
     @staticmethod
     def compute_sequence_of_distances_between_state_seq(primary_sc_state_seq: np.array,
