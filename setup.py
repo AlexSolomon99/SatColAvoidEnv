b2fa27153782ece76656b8a45d2c514312e50765
Alex <alexandru.solomon9999@gmail.com>
2025-03-16 16:54:17 +0200

    Updated the setup version

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ from setuptools import setup
 
 setup(
     name="gym_satellite_ca",
-    version="0.1.0",
-    install_requires=[
-        "python==3.11.8",
-        "gymnasium==0.29.1",
-        "orekit==12.0.1",
-        "numpy==1.26.4"
-    ],
+    version="0.1.1",
+    install_requires=[],
     packages=["gym_satellite_ca", "gym_satellite_ca.envs"],
     include_package_data=True
 )
