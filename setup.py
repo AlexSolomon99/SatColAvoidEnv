23ddafc0ea11cff6f674167de98b8c3e66dc38ed
Alex <alexandru.solomon9999@gmail.com>
2025-03-16 17:22:01 +0200

    Updated setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="gym_satellite_ca",
-    version="0.1.1",
+    version="0.2.0",
     install_requires=[],
     packages=["gym_satellite_ca", "gym_satellite_ca.envs"],
     include_package_data=True
